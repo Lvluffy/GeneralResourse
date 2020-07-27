536430525593309b182902c6af0d38ed9db3758c
lvlufei <lvlufei@xiaomi.com>
2020-07-27 11:20:49 +0800

    加入测试

--- app/src/main/java/com/luffy/generalresourse/MainActivity.java
+++ app/src/main/java/com/luffy/generalresourse/MainActivity.java
@@ -1,13 +1,25 @@
 package com.luffy.generalresourse;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView txt_next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt_next = findViewById(R.id.txt_next);
+        txt_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, OtherActivity.class));
+            }
+        });
     }
 }
